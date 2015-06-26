fb3756b4cb91f6ea76c8dfe843c4476b113bd5af
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-06-26 15:42:45 -0400

    add basic validation script

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 function validateLogin () {
-  // var username = document.getElementbyId()
+  var username = document.loginForm.username.value
+  var password = document.loginForm.password.value
+  var pass_format = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (username == "") {
+    console.log("Error: Username can't be blank!");
+    return false;
+  } else if (pass_format.test(password) != true) {
+    console.log("Error: Password must have at least 6 characters and a combination of lowercase and capital letters");
+    return false;
+  } else {
+    return true;
+  }
 }
